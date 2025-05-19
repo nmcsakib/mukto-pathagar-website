51f44caf08d9e889b203649911c880d2c158d314
nmcsakib <nmcsakib.1@gmail.com>
2025-05-20 01:30:22 +0600

    added navbar section

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,25 +3,65 @@ import React from 'react';
 const Navbar = () => {
     return (
         <>
-           <div className="navbar bg-base-100 shadow-sm">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none">
+           <div className="navbar flex justify-between">
+ 
+  <div className="hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
-      <li>
+       <li>
         <details>
-          <summary>Parent</summary>
+          <summary>Book List</summary>
           <ul className="bg-base-100 rounded-t-none p-2">
             <li><a>Link 1</a></li>
             <li><a>Link 2</a></li>
           </ul>
         </details>
       </li>
+       <li>
+        <details>
+          <summary>Members</summary>
+          <ul className="bg-base-100 rounded-t-none p-2">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li>
+       <li>
+        <details>
+          <summary>Events</summary>
+          <ul className="bg-base-100 rounded-t-none p-2">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li>
+       <li>
+        <details>
+          <summary>Committee</summary>
+          <ul className="bg-base-100 rounded-t-none p-2">
+            <li><a>Link 1</a></li>
+            <li><a>Link 2</a></li>
+          </ul>
+        </details>
+      </li>
+    
     </ul>
   </div>
-</div>
+  <div className="">
+    
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>Homepage</a></li>
+        <li><a>Portfolio</a></li>
+        <li><a>About</a></li>
+      </ul>
+    </div>
+  </div>
+  </div>
         </>
     );
 };
