d425b4676f08abcc371c6b2fcd0e199a5d1d08cb
nmcsakib <nmcsakib.1@gmail.com>
2025-05-20 01:17:05 +0600

    initialized react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+   <main className="container mx-auto px-8">
+    <Navbar></Navbar>
+   </main>
+    </>
+  )
+}
+
+export default App
